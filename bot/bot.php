9f96c1dff5a5051b47db31a2403b541caaca046e
obed011 <158543664+obed011@users.noreply.github.com>
2024-11-15 08:51:11 -0600

    bot.php

--- bot/bot.php
+++ bot/bot.php
@@ -1,13 +1,16 @@
 <?php
+require '../vendor/autoload.php';
 
-$token = '7595311454:AAEDtJtrJUEu6NxK3gtxSRzbWau9YKgMnHo';
+$token = '8137876917:AAFyuuoH2P6OTWKOi8HmPkL-xTNYlgfHEuQ';
 $apiURL = "https://api.telegram.org/bot$token/";
 
+$client = new \GuzzleHttp\Client();
+
 // Datos de conexión a la base de datos
-define('SERVIDOR', 'shinygo_db');
-define('USUARIO', 'shinygo_user');
+define('SERVIDOR', 'pagomoviles_db');
+define('USUARIO', 'pagomoviles_user');
 define('PASSWORD', 'admin123');
-define('BD', 'ShinyGo');
+define('BD', 'pagomovil_es');
 
 $servidor = "mysql:dbname=" . BD . ";host=" . SERVIDOR;
 
@@ -19,134 +22,227 @@ try {
     echo "Connection failed!";
 }
 
-$sql = "SELECT s.nomServicio, s.descripcion, s.precio, s.imagen, p.descuento 
-        FROM servicios s
-        LEFT JOIN promociones p ON s.id = p.servicio_id";
-$stmt = $pdo->prepare($sql);
-$stmt->execute();
-$servicios = $stmt->fetchAll(PDO::FETCH_ASSOC);
+// Obtener las actualizaciones del bot
+$update = file_get_contents("php://input");
+$updateArray = json_decode($update, true);
 
+if (isset($updateArray["message"])) {
+    $chatId = $updateArray["message"]["chat"]["id"];
+    $message = $updateArray["message"]["text"];
 
-$horarios = [
-    "08:00 - 09:00",
-    "09:00 - 10:00",
-    "10:00 - 11:00",
-    "11:00 - 12:00",
-    "13:00 - 14:00",
-    "14:00 - 15:00",
-    "15:00 - 16:00",
-    "16:00 - 17:00"
-];
+    // Responder al mensaje recibido
+    if ($message == "/start") {
+        $message = "¡Bienvenido al bot de PagoMovilES! ¿En qué puedo ayudarte? \n - Realizar pago /pagar \n - Ayuda /ayuda";
+        sendMessage($chatId, $message);
+    } else if ($message == "/pagar") {
+        $message = "Por favor digita tu correo electrónico para continuar";
+        sendMessage($chatId, $message);
+    } else if (filter_var($message, FILTER_VALIDATE_EMAIL)) {
+        //guardar correo en la base de datos
+        $email = $message;
+        //verificar si el correo esta asociado a un usuario
+        $query = "SELECT * FROM usuarios WHERE correo = :email";
+        $stmt = $pdo->prepare($query);
+        $stmt->bindParam(':email', $email);
+        $stmt->execute();
 
+        if ($stmt->rowCount() == 1) {
+            // Verificar si el correo ya existe en la base de datos
+            $query = "SELECT * FROM variables WHERE id = :chatId";
+            $stmt = $pdo->prepare($query);
+            $stmt->bindParam(':chatId', $chatId);
+            $stmt->execute();
 
-// Obtener el contenido de la solicitud
-$content = file_get_contents("php://input");
-$update = json_decode($content, true);
-
-if (!$update) {
-    exit;
-}
-
-// Verificar si es un mensaje o una consulta de devolución de llamada
-if (isset($update['message'])) {
-    // Obtener el ID del chat y el mensaje enviado
-    $chatId = $update['message']['chat']['id'];
-    $message = $update['message']['text'];
-
-    if (!$message) {
-        sendMessage($chatId, "Sorry, I did not understand your message.");
-        exit;
-    }
-
-    if ($message == '/start') {
-        $keyboard = [
-            'inline_keyboard' => [
-                [['text' => 'Consultar Servicios', 'callback_data' => 'Consultar servicios']],
-                [['text' => 'Consultar Disponibilidad', 'callback_data' => 'Consultar disponibilidad']]
-            ]
-        ];
-        sendMessageWithKeyboard($chatId, "Bienvenido al bot de consultas de Shine&Go\n\nSelecciona una opcion:", $keyboard);
-    }
-    // Verificar si el mensaje es un comando para consultar un servicio
-    foreach ($servicios as $servicio) {
-        if ($message == '/' . str_replace(' ', '_', $servicio['nomServicio'])) {
-            $message = "📛 Nombre: " . $servicio['nomServicio'] . "\n";
-            $message .= "📝 Descripción: " . $servicio['descripcion'] . "\n";
-            $message .= "💲 Precio: $" . $servicio['precio'] . "\n";
-            if($servicio['descuento'] != null) {
-                $message .= "🔥 Descuento: " . $servicio['descuento'] . "%\n";
-                $message .= "💲 Precio con descuento: $" . number_format(($servicio['precio'] - ($servicio['precio'] * $servicio['descuento'] / 100)), 2) . "\n";
-            }
-            // Enviar la imagen del servicio
-            $pathPhoto = __DIR__ . '/../storage/app/public/' . $servicio['imagen'];
-            sendPhoto($chatId, $message, $pathPhoto);
-        }
-    }
-
-    for($i = 1; $i <= 7; $i++) {
-        $fecha = date('Y_m_d', strtotime("+$i day"));
-        $fecha2 = date('Y-m-d', strtotime("+$i day"));
-        if ($message == "/$fecha") {
-            $citas = obtenerCitas($pdo, $fecha2);
-            $message = "Citas disponibles para el día $fecha2:\n\n";
-            foreach ($horarios as $horario) {
-                $ocupado = false;
-                foreach ($citas as $cita) {
-                    if ($cita['hora_cita'] == $horario) {
-                        $ocupado = true;
-                        break;
-                    }
-                }
-                if (!$ocupado) {
-                    $message .= "✅ $horario\n";
+            if ($stmt->rowCount() == 1) {
+                // Si el correo ya existe, actualizarlo
+                $query = "UPDATE variables SET correo = :email , servicio = 'n/a', monto = 0 WHERE id = :chatId";
+                $stmt = $pdo->prepare($query);
+                $stmt->bindParam(':email', $email);
+                $stmt->bindParam(':chatId', $chatId);
+                if ($stmt->execute()) {
+                    $message = "Selecciona el servicio que deseas pagar \n - Luz /luz \n - Agua /agua \n - Teléfono /telefono \n - Internet /internet";
+                    sendMessage($chatId, $message);
                 } else {
-                    $message .= "❌ $horario\n";
+                    $message = "Hubo un error al actualizar tu correo. Por favor, intenta nuevamente.";
+                    sendMessage($chatId, $message);
+                }
+            } else {
+                $servicio = "n/a";
+                $monto = 0;
+                // Si el correo no existe, insertarlo
+                $query = "INSERT INTO variables (id, correo, servicio, monto) VALUES (:chatId, :email, :servicio, :monto)";
+                $stmt = $pdo->prepare($query);
+                $stmt->bindParam(':chatId', $chatId);
+                $stmt->bindParam(':email', $email);
+                $stmt->bindParam(':servicio', $servicio);
+                $stmt->bindParam(':monto', $monto);
+                if ($stmt->execute()) {
+                    $message = "Selecciona el servicio que deseas pagar \n - Luz /luz \n - Agua /agua \n - Teléfono /telefono \n - Internet /internet";
+                    sendMessage($chatId, $message);
+                } else {
+                    $message = "Hubo un error al registrar tu correo. Por favor, intenta nuevamente.";
+                    sendMessage($chatId, $message);
                 }
             }
+        } else {
+
+            $message = "El correo no esta asociado a ningun usuario.";
             sendMessage($chatId, $message);
         }
-    }
-    
-} elseif (isset($update['callback_query'])) {
-    // Obtener el ID del chat y los datos de devolución de llamada
-    $chatId = $update['callback_query']['message']['chat']['id'];
-    $callbackData = $update['callback_query']['data'];
+    } else if ($message == "/luz" || $message == "/agua" || $message == "/telefono" || $message == "/internet") {
+        //guardar servicio en la base de datos
+        $servicio = ltrim($message, '/');
+        // Verificar si el servicio ya existe en la base de datos
+        $query = "SELECT * FROM variables WHERE id = :chatId";
+        $stmt = $pdo->prepare($query);
+        $stmt->bindParam(':chatId', $chatId);
+        $stmt->execute();
 
-    if ($callbackData == 'Consultar servicios') {
-        $message = "Estos son nuestros servicios disponibles:\n\n";
-        foreach ($servicios as $servicio) {
-            $message .= "✅ " . $servicio['nomServicio'] . "\t\t$" . $servicio['precio'] . "\n";
-            $message .= "ℹ️ Más información: /" . str_replace(' ', '_', $servicio['nomServicio']) . "\n\n";
+        if ($stmt->rowCount() == 1) {
+            // Si el servicio ya existe, actualizarlo
+            $query = "UPDATE variables SET servicio = :servicio WHERE id = :chatId";
+            $stmt = $pdo->prepare($query);
+            $stmt->bindParam(':servicio', $servicio);
+            $stmt->bindParam(':chatId', $chatId);
+            if ($stmt->execute()) {
+                $message = "Por favor digita el monto a pagar";
+                sendMessage($chatId, $message);
+            } else {
+                $message = "Hubo un error al actualizar tu servicio. Por favor, intenta nuevamente.";
+                sendMessage($chatId, $message);
+            }
+        } else {
+            $message = "No has proporcionado un correo electrónico.";
+            sendMessage($chatId, $message);
         }
+    } else if (is_numeric($message)) {
+        //guardar monto en la base de datos
+        $monto = $message;
+        //verificar si el monto es mayor a 0
+        if ($monto > 0) {
+            // Verificar si el monto ya existe en la base de datos
+            $query = "SELECT * FROM variables WHERE id = :chatId";
+            $stmt = $pdo->prepare($query);
+            $stmt->bindParam(':chatId', $chatId);
+            $stmt->execute();
 
+            if ($stmt->rowCount() == 1) {
+                // Si el monto ya existe, actualizarlo
+                $query = "UPDATE variables SET monto = :monto WHERE id = :chatId";
+                $stmt = $pdo->prepare($query);
+                $stmt->bindParam(':monto', $monto);
+                $stmt->bindParam(':chatId', $chatId);
+                if ($stmt->execute()) {
+                    $query = "SELECT * FROM variables WHERE id = :chatId";
+                    $stmt = $pdo->prepare($query);
+                    $stmt->bindParam(':chatId', $chatId);
+                    $stmt->execute();
+                    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+                    $message = "Por favor confirma tu pago \n Correo: " . $row['correo'] . "\n Servicio: " . $row['servicio'] . "\n Monto: " . $row['monto'] . "\n ¿Deseas confirmar tu pago? \n /confirmar \n /cancelar";
+                    sendMessage($chatId, $message);
+                } else {
+                    $message = "Hubo un error al actualizar tu monto. Por favor, intenta nuevamente.";
+                    sendMessage($chatId, $message);
+                }
+            } else {
+                $message = "No has proporcionado un correo electrónico.";
+                sendMessage($chatId, $message);
+            }
+        } else {
+            $message = "Por favor digita un monto mayor a 0.";
+            sendMessage($chatId, $message);
+        }
+    } else if ($message == "/confirmar") {
+        // Verificar si el pago ya existe en la base de datos
+        $query = "SELECT * FROM variables WHERE id = :chatId";
+        $stmt = $pdo->prepare($query);
+        $stmt->bindParam(':chatId', $chatId);
+        $stmt->execute();
+        $valores = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        $sql = "SELECT nombre_completo FROM usuarios WHERE correo = :correo";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(':correo', $valores['correo']);
+        $stmt->execute();
+        $usuario = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        $response = $client->request('POST', 'https://api.opennode.com/v1/charges', [
+            'body' => '{
+            "amount": ' . $valores['monto'] . ',
+            "currency": "USD",
+            "description": "Pago de ' . $valores['servicio'] . '",
+            "callback_url": "https://bot.pagoes.line.pm/bot.php",
+            "customer_name": "' . $usuario['nombre_completo'] . '",
+            "customer_email": "' . $valores['correo'] . '",
+            "order_id": "' . $chatId . '+' . time() . '",
+            "notify_receiver": true
+            }',
+            'headers' => [
+                'Content-Type' => 'application/json',
+                'accept' => 'application/json',
+                'Authorization' => '73123932-1bf0-415e-b03a-b01a39b62cf9'
+            ],
+        ]);
+        $response = json_decode($response->getBody(), true);
+
+
+        //extrer el lightning_invoice
+        $lightning_invoice = $response['data']['lightning_invoice']['payreq'];
+
+        //convertir a mayusculas
+        $lightning_invoice = strtoupper($lightning_invoice);
+
+        $urlQr = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" . urlencode($lightning_invoice);
+
+        //descargar la imagen
+        $pathQr = __DIR__ . '/../storage/app/public/' . $chatId . '.png';
+        file_put_contents($pathQr, file_get_contents($urlQr));
+
+        $detalles = "Los detalles de tu pago son: \n Correo: " . $valores['correo'] . "\n Servicio: " . $valores['servicio'] . "\n Monto: " . $valores['monto'] . "\n\n Escanea el código QR para realizar tu pago.";
+        //enviar la imagen
+        sendPhoto($chatId, $detalles, $pathQr);
+        
+    } else if ($message == "/cancelar") {
+        $query = "DELETE FROM variables WHERE id = :chatId";
+        $stmt = $pdo->prepare($query);
+        $stmt->bindParam(':chatId', $chatId);
+        $stmt->execute();
+        if ($stmt->rowCount() > 0) {
+            $message = "Tu pago ha sido cancelado, hemos eliminado tus datos.";
+            sendMessage($chatId, $message);
+        } else {
+            $message = "No hay registro de pago para cancelar.";
+            sendMessage($chatId, $message);
+        }
+    } else if ($message == "/ayuda") {
+        $message = "¡Bienvenido al bot de PagoMovilES! \n Para realizar un pago debes de seguir las siguientes instrucciones: \n\n 1. Digita /pagar \n 2. Ingresa tu correo electrónico \n 3. Selecciona el servicio a pagar \n 4. Ingresa el monto a pagar \n 5. Confirma tu pago";
         sendMessage($chatId, $message);
-    } elseif ($callbackData == 'Consultar disponibilidad') {
-        $message = "Puedes ver las disponibilidades para esta semana: \n\n";
-        $daysOfWeek = [];
-        for ($i = 1; $i <= 7; $i++) {
-            $daysOfWeek[] = "📅 /" . date('Y_m_d', strtotime("+$i day"));
-        }
-        $message .= implode("\n\n", $daysOfWeek);
+    } else {
+        $message = "Respuesta no válida. Por favor, intenta nuevamente.";
         sendMessage($chatId, $message);
     }
 }
 
 // Enviar un mensaje con un teclado personalizado
-function sendMessageWithKeyboard($chatId, $message, $keyboard) {
+function sendMessageWithKeyboard($chatId, $message, $keyboard)
+{
     global $apiURL;
     $url = $apiURL . "sendMessage?chat_id=" . $chatId . "&text=" . urlencode($message) . "&reply_markup=" . json_encode($keyboard);
     file_get_contents($url);
 }
 
 // Enviar un mensaje simple
-function sendMessage($chatId, $message) {
+function sendMessage($chatId, $message)
+{
     global $apiURL;
     $url = $apiURL . "sendMessage?chat_id=" . $chatId . "&text=" . urlencode($message);
     file_get_contents($url);
 }
 
-// Enviar una foto
-function sendPhoto($chatId, $caption, $pathPhoto) {
+// Enviar una foto desde un archivo local
+function sendPhoto($chatId, $caption, $pathPhoto)
+{
     global $apiURL;
     $url = $apiURL . "sendPhoto";
 
@@ -167,12 +263,10 @@ function sendPhoto($chatId, $caption, $pathPhoto) {
     return $output;
 }
 
-function obtenerCitas($pdo, $fecha) {
-    $sql = "SELECT fecha_cita, hora_cita FROM citas WHERE fecha_cita = :fecha";
-    $stmt = $pdo->prepare($sql);
-    $stmt->bindParam(':fecha', $fecha);
-    $stmt->execute();
-    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+//enviar foto desde una url
+function sendPhotoFromUrl($chatId, $caption, $urlPhoto)
+{
+    global $apiURL;
+    $url = $apiURL . "sendPhoto?chat_id=" . $chatId . "&caption=" . urlencode($caption) . "&photo=" . $urlPhoto;
+    file_get_contents($url);
 }
-
-?>
